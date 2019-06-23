586a1703e826d74130415a744014f69fbf5af0f7
Quinton Lucyk <quinton_lucyk@yahoo.com>
2019-06-23 17:58:53 -0400

    adding input, next is api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import Input from 'react-bootstrap/Input';
+import { InputGroup, FormControl, Button } from 'react-bootstrap';
+import './App.scss';
 
 function App() {
+
+  // const loadCommentsFromServer = () => {
+  //   fetch('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&apikey=DG61VKY2XF1OVBWJ').then(function (response) {
+  //     console.log(response);
+  //   });
+  // };
+
   return (
-    <div className="App">
-      <Input />
-      <span class="oi oi-icon-name" title="icon name" aria-hidden="true"></span>
+    <div className="App container">
+      <div className="row justify-content-center m-4">
+        <div className="col-6">
+          <InputGroup >
+            <FormControl
+              placeholder="Symbol"
+              aria-label="Symbol"
+              id="symbol-search"
+            />
+            <InputGroup.Append>
+              <Button >Search</Button>
+            </InputGroup.Append>
+          </InputGroup>
+        </div>
+      </div>
     </div>
   );
 }
