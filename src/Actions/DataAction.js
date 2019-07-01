ab8c0d6e8fb4b86193028a52e10d5ed63f7105cb
Quinton Lucyk <quinton_lucyk@yahoo.com>
2019-07-01 16:36:41 -0400

    took out comments

--- src/Actions/DataAction.js
+++ src/Actions/DataAction.js
@@ -27,24 +27,4 @@ export const fetchData = symbol => async dispatch => {
       growth: growth
     }
   });
-
-  // console.log("fetching");
-  // const profile = await profileCall(symbol);
-  // const metrics = await metricsCall(symbol);
-  // const balance = await balanceCall(symbol);
-  // const income = await incomeCall(symbol);
-  // const cash = await cashCall(symbol);
-  // const growth = await growthCall(symbol);
-  // console.log("growthhh");
-  // dispatch({
-  //   type: FETCH_DATA,
-  //   payload: {
-  //     profile: profile,
-  //     metrics: metrics,
-  //     balance: balance,
-  //     income: income,
-  //     cash: cash,
-  //     growth: growth
-  //   }
-  // });
 };
