6b940440217ac0089bd70d44d3a8e710529aad17
Quinton Lucyk <quinton_lucyk@yahoo.com>
2019-07-01 16:35:53 -0400

    got async working

--- src/Actions/DataAction.js
+++ src/Actions/DataAction.js
@@ -9,7 +9,7 @@ import {
   growthCall
 } from "../APICalls/FinancialModellingPrep";
 
-export const fetchData = async symbol => async dispatch => {
+export const fetchData = symbol => async dispatch => {
   const profile = await profileCall(symbol);
   const metrics = await metricsCall(symbol);
   const balance = await balanceCall(symbol);
@@ -27,4 +27,24 @@ export const fetchData = async symbol => async dispatch => {
       growth: growth
     }
   });
+
+  // console.log("fetching");
+  // const profile = await profileCall(symbol);
+  // const metrics = await metricsCall(symbol);
+  // const balance = await balanceCall(symbol);
+  // const income = await incomeCall(symbol);
+  // const cash = await cashCall(symbol);
+  // const growth = await growthCall(symbol);
+  // console.log("growthhh");
+  // dispatch({
+  //   type: FETCH_DATA,
+  //   payload: {
+  //     profile: profile,
+  //     metrics: metrics,
+  //     balance: balance,
+  //     income: income,
+  //     cash: cash,
+  //     growth: growth
+  //   }
+  // });
 };
