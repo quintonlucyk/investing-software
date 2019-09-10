0ea6b201a912aa01d44d8025927f2e5234a93bb7
quintonlucyk <quinton_lucyk@yahoo.com>
2019-09-10 14:42:46 -0400

    1st index of array coded

--- src/Components/DisplayEllenTableData.js
+++ src/Components/DisplayEllenTableData.js
@@ -8,8 +8,8 @@ class DisplayEllenTableData extends React.Component {
       return null;
     } else if (this.props.historicalAdjusted.Error === undefined) {
       let tbody = new Array(11);
-      for (let entry of tbody) {
-        entry = new Array(6);
+      for (let i = 0; i < tbody.length; ++i) {
+        tbody[i] = new Array(6);
       }
       /**
        * tbody will follow structure as such:
@@ -18,24 +18,82 @@ class DisplayEllenTableData extends React.Component {
        * .
        * .
        * [date, dividend, low share, high yield, high share, low yield]]
-       *
        */
+      let pos0entered = false,
+        pos1entered = false,
+        pos2entered = false,
+        pos3entered = false,
+        pos4entered = false,
+        pos5entered = false,
+        pos6entered = false,
+        pos7entered = false,
+        pos8entered = false,
+        pos9entered = false,
+        pos10entered = false;
       let tempYearOfDate = new Date();
       tempYearOfDate = tempYearOfDate.getYear() + 1900;
       for (let tempval in this.props.historicalAdjusted[
         "Time Series (Daily)"
       ]) {
-        // come up with new traverse here
-
         if (tempval.substring(0, 4) < tempYearOfDate - 10) {
           break;
         } else {
           let entryInArray = tempYearOfDate - tempval.substring(0, 4);
-          console.log(entryInArray);
-          console.log(tbody.length);
+          //need to make this into helper function
+          if (entryInArray === 0) {
+            if (pos0entered === false) {
+              //initiate share values here
+
+              tbody[0][0] = tempval.substring(0, 4); //can use tbody[0] because of above entryInArray === 0 check
+              tbody[0][1] = this.props.historicalAdjusted[
+                "Time Series (Daily)"
+              ][tempval]["7. dividend amount"];
+              tbody[0][2] = this.props.historicalAdjusted[
+                "Time Series (Daily)"
+              ][tempval]["3. low"];
+              tbody[0][4] = this.props.historicalAdjusted[
+                "Time Series (Daily)"
+              ][tempval]["2. high"];
+              tbody[0][1] = parseFloat(
+                this.props.historicalAdjusted["Time Series (Daily)"][tempval][
+                  "7. dividend amount"
+                ]
+              );
+              pos0entered = true;
+            } else {
+              /**
+               * tbody[0][2] = tempval["3. low"];
+               * tbody[0][4] = tempval["2. high"];
+               */
+              if (
+                this.props.historicalAdjusted["Time Series (Daily)"][tempval][
+                  "3. low"
+                ] < tbody[0][2]
+              ) {
+                tbody[0][2] = this.props.historicalAdjusted[
+                  "Time Series (Daily)"
+                ][tempval]["3. low"];
+              }
+              if (
+                this.props.historicalAdjusted["Time Series (Daily)"][tempval][
+                  "2. high"
+                ] < tbody[0][4]
+              ) {
+                tbody[0][4] = this.props.historicalAdjusted[
+                  "Time Series (Daily)"
+                ][tempval]["2. high"];
+              }
+              tbody[0][1] += parseFloat(
+                this.props.historicalAdjusted["Time Series (Daily)"][tempval][
+                  "7. dividend amount"
+                ]
+              );
+            }
+          }
         }
-        console.log(tempval);
       }
+      console.log(tbody);
+
       // for (let )
       // if (
       //   this.props.metrics.metrics[i] !== undefined &&
